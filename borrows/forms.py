e15bb9f438040d9e416890b59e4d75146557367b
Yuchen Liu <steny138@gmail.com>
2017-07-06 01:38:01 +0800

    調整form日期欄位的顯示與格式

--- borrows/forms.py
+++ borrows/forms.py
@@ -8,8 +8,10 @@ from django.forms.extras import SelectDateWidget
 from django import forms
 
 from home import settings
+from home.widget import DateTimePicker
 from borrows.models import Borrower
 from sim.models import Sim
+from datetime import datetime
 
 class BorrowForm(ModelForm):
     number = forms.ModelChoiceField(
@@ -24,18 +26,20 @@ class BorrowForm(ModelForm):
         required=True)
 
     purpose_other = forms.CharField(label='其他原因', required=False)
+    # rent_date = forms.DateField(label="租借日",
+    #     widget=SelectDateWidget(
+    #         empty_label=("Choose Year", "Choose Month", "Choose Day"),
+    #     ), 
+    #     required=True
+    # )
     rent_date = forms.DateField(label="租借日",
-        widget=SelectDateWidget(
-            empty_label=("Choose Year", "Choose Month", "Choose Day"),
-        ), 
-        required=True
-    )
+                                widget=DateTimePicker(options={"format": "YYYY/MM/DD"}),
+                                input_formats=["%Y/%m/%d"],
+                                initial=datetime.now())
+
     borrow_date = forms.DateField(label="到期日",
-        widget=SelectDateWidget(
-            empty_label=("Choose Year", "Choose Month", "Choose Day"),
-        ), 
-        required=True
-    )
+                                widget=DateTimePicker(options={"format": "YYYY/MM/DD"}),
+                                input_formats=["%Y/%m/%d"])
 
     class Meta:
         model = Borrower
--- se3_borrows/settings.py
+++ se3_borrows/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 
-DATABASES['default'].update(db_from_env)
+# DATABASES['default'].update(db_from_env)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
