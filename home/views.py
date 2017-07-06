2bd45be4aa5a8c0477da751a0f1f02aa027e7ff8
Yuchen Liu <steny138@gmail.com>
2017-07-07 01:45:28 +0800

    index display with sim card dashboard.

--- home/views.py
+++ home/views.py
@@ -1,12 +1,94 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import auth
 from datetime import datetime
 from django.http import HttpResponse
 
+from datetime import datetime
+
+from sim.models import Sim
+from borrows.models import Borrower
+
+import json
+
 def index(request):
+    # 取得Sim卡全部資料
+    simcards = Sim.objects.all()
+
+    # 初始化圖表
+    chart_rows = []
+    chart_row = []
+
+    # 加入 電信商圓餅圖
+    chart_row.append(__operate_chart(simcards))
+
+    # 加入 借用狀態圓餅圖
+    chart_row.append(__sim_status_chart(simcards))
+
+    chart_rows.append(chart_row)
+
     return render(request, 'index.html', {
-        'current_time': datetime.now().strftime('%Y-%m-%d(%a) %H:%M:%S'),
+        'chart_rows': chart_rows
     })
+
+def __operate_chart(simcards):
+    chart_row = []
+
+    # group by user, show the user rent sim cards information.
+    res = {}
+    for item in simcards:
+        res.setdefault(item.get_operate_display(), []).append(item)
+
+
+    chart = {}
+    chart["size"] = 5
+    chart["title"] = "Sim卡"
+    chart["desp"] = "電信商分佈數量"
+    chart["id"] = "sim_operate"
+    chart["time"] = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    chart["items"] = []
+    
+    count = 1
+    labels = []
+    series = []
+    for key in res:
+        labels.append(key)
+        series.append(len(res[key]))
+        chart["items"].append({ "name":key, "color":count})
+        count += 1
+
+    chart["labels"] = json.dumps(labels)
+    chart["series"] = json.dumps(series)
+    
+    return chart
+
+def __sim_status_chart(simcards):
+    
+    # group by user, show the user rent sim cards information.
+    res = {}
+    for item in simcards:
+        res.setdefault(item.get_status_display(), []).append(item)
+    chart = {}
+    chart["size"] = 5
+    chart["title"] = "Sim卡"
+    chart["desp"] = "借用狀態分佈"
+    chart["id"] = "sim_status"
+    chart["time"] = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    chart["items"] = []
+    
+    count = 1
+    labels = []
+    series = []
+    for key in res:
+        labels.append(key)
+        series.append(len(res[key]))
+        chart["items"].append({ "name":key, "color":count})
+        count += 1
+
+    chart["labels"] = json.dumps(labels)
+    chart["series"] = json.dumps(series)
+    return chart
 
 def login(request):
     print "login"
